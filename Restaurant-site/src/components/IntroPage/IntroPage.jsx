a2b7cb3150536f2172b3166a998a9dc8bd5d6439
Igor Izotikov PC <izotikov.igor1@yandex.ru>
2024-08-25 13:40:20 +0300

    Reboot in case all bad

--- Restaurant-site/src/components/IntroPage/IntroPage.jsx
+++ Restaurant-site/src/components/IntroPage/IntroPage.jsx
@@ -6,14 +6,19 @@ const IntroPage = () => {
         <div className="intro-page">
             <div className="intro-page__text-block">
                 <div className="intro-page__text-block--heading">
-                    Healthy Eating is important part of lifestyle
+                    Healthy Eating <br/>is important <br/>part of lifestyle
                 </div>
                 <div className="intro-page__text-block--regular">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu
+                    Lorem ipsum dolor sit amet, consectetur adipiscing <br/>elit. Neque congue arcu
                 </div>
             </div>
             <div className="intro-page__scroll">Scroll --------------------------</div>
             <img src="../../../public/plate.jfif" alt="plate" id="intro-page__food-photo"/>
+            <div className="intro-page__photo-container">
+                <img src="../../../public/intro-page__food-from-list-1.png" alt="plate with food"/>
+                <img src="../../../public/intro-page__food-from-list-2.png" alt="plate with food"/>
+                <img src="../../../public/intro-page__food-from-list-3.png" alt="plate with food"/>
+            </div>
         </div>
     );
 };
