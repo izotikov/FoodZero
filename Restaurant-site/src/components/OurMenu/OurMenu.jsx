43d416a3822546fb4eaa19c4921c95eb75df0e3d
Igor Izotikov PC <izotikov.igor1@yandex.ru>
2024-08-27 16:13:16 +0300

    Second page completed without image

--- Restaurant-site/src/components/OurMenu/OurMenu.jsx
+++ Restaurant-site/src/components/OurMenu/OurMenu.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import './OurMenu.css';
 
 const OurMenu = () => {
+
+    const [menuDescription, setMenuDescription] = useState('This is a section of your menu. Give your section a brief description');
+
+    const [menuItems, setMenuItems] = useState([]);
+    useEffect(() => {
+        fetch("/API.json")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setMenuItems(data.MenuItems);
+            });
+    }, []);
+
     return (
-        <div>
-            hee
+        <div className="our-menu">
+            <div className="our-menu__top-block">
+                <div className="our-menu__top-block__heading">Our menu</div>
+                <div className="our-menu__top-block__description">{menuDescription}</div>
+            </div>
+            <div className="our-menu__menu-positions">
+                {menuItems.map((elem) => {
+                    return (
+                        <div key={elem.No} className="our-menu__menu-positions__container">
+                            <div className="our-menu__menu-positions__container--price">{elem.Price}</div>
+                            <div className="our-menu__menu-positions__container--dish-name">{elem.Name}</div>
+                            <div className="our-menu__menu-positions__container--dish-description"> {elem.Description}</div>
+                        </div>
+
+                        )
+                })}
+            </div>
         </div>
     );
 };
